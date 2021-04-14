c3ac8c04149af2db6211a302a4753dc8ecae1602
Paulinha <pvgueiros@gmail.com>
2021-04-14 14:59:25 -0300

    Initial Commit

---
+++ SegmentedPaging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedPaging
+//
+//  Created by Paula Vasconcelos Gueiros on 14/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
